71adbc37924b8bd0cc4c13aa34ca93ddd9022b93
Brett Snipes <43290072+bcsnipes@users.noreply.github.com>
2026-01-05 16:19:42 -0500

    Add new screens

--- App.tsx
+++ App.tsx
@@ -17,21 +17,41 @@ function HomeScreen(): React.JSX.Element {
   );
 }
 
-// Logs Screen Component
-function LogsScreen(): React.JSX.Element {
+// Couple Screen Component
+function CoupleScreen(): React.JSX.Element {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Logs</Text>
+      <Text style={styles.text}>Couple</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-// Settings Screen Component
-function SettingsScreen(): React.JSX.Element {
+// Actions Screen Component
+function ActionsScreen(): React.JSX.Element {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Settings</Text>
+      <Text style={styles.text}>Actions</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+// Price Screen Component
+function PriceScreen(): React.JSX.Element {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Price</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+// History Screen Component
+function HistoryScreen(): React.JSX.Element {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>History</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -43,8 +63,10 @@ export default function App(): React.JSX.Element {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Logs" component={LogsScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Couple" component={CoupleScreen} />
+        <Tab.Screen name="Actions" component={ActionsScreen} />
+        <Tab.Screen name="Price" component={PriceScreen} />
+        <Tab.Screen name="History" component={HistoryScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
